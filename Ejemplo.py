9e5b32d630c4bd12986414f5553e2f3ced78d00e
MitchFrdz <sara.quintero@tectijuana.edu.mx>
2017-10-12 12:31:13 -0700

    Patron Singleton

--- Ejemplo.py
+++ Ejemplo.py
@@ -1,1 +1,30 @@
- 
+#class Empresa(object):
+
+#class Departamento(Empresa):
+
+class Puesto(object):#Empresa):
+    #instances = {}
+    #def __call__(cls, *args, **kwargs):
+        #if cls not in cls._instances:
+            #cls._instances[cls]= super(Puesto, cls).__call__(*args, **kwargs)
+            #return cls._instances[cls]
+    _instance = None
+    def __new__(class_, *args, **kwargs):
+        if not isinstance(class_._instance, class_):
+            class_._instance = object.__new__(class_, *args, **kwargs)
+        return class_._instance   
+    
+class Empleado(Puesto): #object
+   # __metaclass__= Puesto
+        
+
+    
+x = Puesto('DFGS345')
+print (x)
+y = Puesto('DFGS345')
+print (y)
+z = Puesto('DFVE345')
+print (z)
+print(x)
+print(y)
+print(z)
